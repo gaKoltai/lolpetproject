748a88443c132822b28a99cdb825b9ba53596735
Gergely Koltai <kgalmos@gmail.com>
2019-11-18 18:02:56 +0100

    ???

--- src/components/SummonerStats.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import uuidv4 from "uuid/v4";
-import Ranked from "./Ranked";
-
-export default function SummonerStats({ stats }) {
-return <div>{console.log(stats)}</div>
-}
