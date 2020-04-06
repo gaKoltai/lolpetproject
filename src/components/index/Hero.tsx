ccbf3c3dbb99f27503311767a71b37326cf4efcb
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-06 17:35:02 +0200

    Created Hero style page background

---
+++ src/components/index/Hero.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const StyledHero = styled.div`
+    height: 94vh;
+    min-width: 100vw;
+    /*background: linear-gradient(to top, rgb(39, 54, 82) 0%, rgb(32, 43, 67) 100%);*/
+    background-image: url(${require("../static/images/backgrounds/runeterra.jpg")});
+    box-shadow: inset 0 0 0 100vmax rgba(32, 43, 67, 0.8);
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 10vw;
+`;
+
+interface Props {
+    children?: ReactNode;
+}
+
+const Hero = (props: Props) => {
+    return <StyledHero>{props.children}</StyledHero>;
+};
+
+export default Hero;
