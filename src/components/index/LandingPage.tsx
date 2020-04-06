d9d6264710356bc89a6d9216d7897e2ab2816d99
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-06 17:35:34 +0200

    Created new indexpage

---
+++ src/components/index/LandingPage.tsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components";
+import SearchWrapper from "../search/SearchWrapper";
+import { Title } from "../Title";
+import { SummonerContext } from "../context/SummonerProvider";
+
+export const Hero = styled.div`
+    min-height: 94vh;
+    width: 100vw;
+    /*background: linear-gradient(to top, rgb(39, 54, 82) 0%, rgb(32, 43, 67) 100%);*/
+    background-image: url(${require("../../static/images/backgrounds/runeterra.jpg")});
+    box-shadow: inset 0 0 0 100vmax rgba(32, 43, 67, 0.8);
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 10vw;
+`;
+
+const LandingPage = () => {
+    const [summoner, setSummoner] = useContext(SummonerContext);
+
+    useEffect(() => {
+        setSummoner("");
+    }, []);
+
+    return (
+        <Hero>
+            <Title />
+            <SearchWrapper large />
+        </Hero>
+    );
+};
+
+export default LandingPage;
