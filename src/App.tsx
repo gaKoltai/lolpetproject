3f553ff19379abea88c4f13476e20b0aa68ef3d6
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-06 17:30:32 +0200

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SummonerLandingPage from "./components/SummonerLandingPage";
 import { SummonerProvider } from "./components/context/SummonerProvider";
 import NavBar from "./components/navigation/NavBar";
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/index/LandingPage";
 import { SearchValueProvider } from "./components/context/SearchValueProvider";
 import { RegionProvider } from "./components/context/RegionProvider";
-import MainPageHero from "./components/MainPageHero";
+import SummonerStatsPage from "./components/match-history/SummonerStatsPage";
 
 const App: React.FC = () => {
     return (
         <Router>
-            <SearchValueProvider>
-                <RegionProvider>
-                    <SummonerProvider>
-                        <NavBar />
+                <SearchValueProvider>
+                    <RegionProvider>
+                        <SummonerProvider>
+                            <NavBar />
 
-                        <Switch>
-                            <Route path="/" exact component={LandingPage} />
-                            <Route path="/summoner/:region/:summonerName" component={SummonerLandingPage} />
-                        </Switch>
-                    </SummonerProvider>
-                </RegionProvider>
-            </SearchValueProvider>
+                            <Switch>
+                                <Route path="/" exact component={LandingPage} />
+                                <Route path="/summoner/:region/:summonerName" component={SummonerStatsPage} />
+                            </Switch>
+                        </SummonerProvider>
+                    </RegionProvider>
+                </SearchValueProvider>
         </Router>
     );
 };
