92bab0cfcb437e8625373d5f5aad87c8bf50a92d
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-06 17:33:51 +0200

    Added missing export

--- src/components/context/SummonerProvider.jsx
+++ src/components/context/SummonerProvider.jsx
@@ -2,7 +2,7 @@ import React, { useState, createContext } from "react";
 
 export const SummonerContext = createContext();
 
-export const SummonerProvider = props => {
+export const SummonerProvider = (props) => {
     const [summoner, setSummoner] = useState();
 
     return <SummonerContext.Provider value={[summoner, setSummoner]}>{props.children}</SummonerContext.Provider>;
