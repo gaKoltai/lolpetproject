0daeb790ca236e773c6b752c297362d247f31342
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-04-07 17:40:46 +0200

    Changed summoner data context' name

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { SummonerProvider } from "./components/context/SummonerProvider";
 import NavBar from "./components/navigation/NavBar";
 import LandingPage from "./components/index/LandingPage";
 import { SearchValueProvider } from "./components/context/SearchValueProvider";
 import { RegionProvider } from "./components/context/RegionProvider";
 import SummonerStatsPage from "./components/match-history/SummonerStatsPage";
+import SummonerDataProvider from "./components/context/SummonerDataProvider";
 
 const App: React.FC = () => {
     return (
         <Router>
+            <SummonerDataProvider>
                 <SearchValueProvider>
                     <RegionProvider>
-                        <SummonerProvider>
-                            <NavBar />
+                        <NavBar />
 
-                            <Switch>
-                                <Route path="/" exact component={LandingPage} />
-                                <Route path="/summoner/:region/:summonerName" component={SummonerStatsPage} />
-                            </Switch>
-                        </SummonerProvider>
+                        <Switch>
+                            <Route path="/" exact component={LandingPage} />
+                            <Route path="/summoner/:region/:summonerName" component={SummonerStatsPage} />
+                        </Switch>
                     </RegionProvider>
                 </SearchValueProvider>
+            </SummonerDataProvider>
         </Router>
     );
 };
