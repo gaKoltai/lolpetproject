f5ae106ecfba101b808fd0206f8864fabf4cb4cd
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-08 20:23:04 +0200

    Removed old hero implementation and changed height property of new

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -1,15 +1,19 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
 
+interface StyleProps {
+    main?: boolean;
+}
+
 const StyledHero = styled.div`
-    height: 94vh;
+    min-height: 94vh;
     min-width: 100vw;
     /*background: linear-gradient(to top, rgb(39, 54, 82) 0%, rgb(32, 43, 67) 100%);*/
-    background-image: url(${require("../static/images/backgrounds/runeterra.jpg")});
+    background-image: url(${require("../../static/images/backgrounds/runeterra.jpg")});
     box-shadow: inset 0 0 0 100vmax rgba(32, 43, 67, 0.8);
     background-size: cover;
     display: flex;
-    flex-direction: row;
+    flex-direction: ${(props: StyleProps) => (props.main ? "column" : "row")};
     justify-content: center;
     align-items: center;
     padding: 0 10vw;
@@ -17,10 +21,11 @@ const StyledHero = styled.div`
 
 interface Props {
     children?: ReactNode;
+    main?: boolean;
 }
 
 const Hero = (props: Props) => {
-    return <StyledHero>{props.children}</StyledHero>;
+    return <StyledHero main={props.main}>{props.children}</StyledHero>;
 };
 
 export default Hero;
--- src/components/index/LandingPage.tsx
+++ src/components/index/LandingPage.tsx
@@ -3,32 +3,17 @@ import styled from "styled-components";
 import SearchWrapper from "../search/SearchWrapper";
 import { Title } from "../Title";
 import { SummonerDataContext } from "../context/SummonerDataProvider";
-
-export const Hero = styled.div`
-    min-height: 94vh;
-    width: 100vw;
-    /*background: linear-gradient(to top, rgb(39, 54, 82) 0%, rgb(32, 43, 67) 100%);*/
-    background-image: url(${require("../../static/images/backgrounds/runeterra.jpg")});
-    box-shadow: inset 0 0 0 100vmax rgba(32, 43, 67, 0.8);
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 0 10vw;
-`;
+import Hero from "./Hero";
 
 const LandingPage = () => {
-
     const [summoner, setSummoner] = useContext(SummonerDataContext);
 
     useEffect(() => {
         setSummoner("");
     }, []);
 
-
     return (
-        <Hero>
+        <Hero main>
             <Title />
             <SearchWrapper large />
         </Hero>
