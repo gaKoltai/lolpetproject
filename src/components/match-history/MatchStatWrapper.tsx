7a4fb44949ea0fac46fa5a84e6f90b19076d75b4
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-08 20:24:34 +0200

    Changed styling

--- src/components/match-history/MatchStatWrapper.tsx
+++ src/components/match-history/MatchStatWrapper.tsx
@@ -4,10 +4,11 @@ import styled from "styled-components";
 const StyledStatWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    width: 20%;
+    font-size: 0.7rem;
     justify-content: center;
-    align-items: center;
+    align-items: left;
     margin: 0.2rem;
+    padding: 0.5rem;
 `;
 
 interface Props {
