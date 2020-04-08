e5ecabe27c5ce926f8dd7d0f6f80c8cfe2a8ef5b
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-08 20:24:23 +0200

    Implemented data fetch with the new object sent from the backend API

--- src/components/match-history/Match.tsx
+++ src/components/match-history/Match.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useContext, Fragment } from "react";
-import { matchSpecificEndpoint, calculateKDA } from "../../static/util/utilities";
-import { MatchInfo, QueueTypes, MatchSpecific, Participant } from "../../static/util/jsonDataInterfaces";
+import { matchSpecificEndpoint } from "../../static/util/utilities";
+import { MatchInfo, QueueTypes, SortedMatchData } from "../../static/util/jsonDataInterfaces";
 import { RegionContext } from "../context/RegionProvider";
 import axios, { AxiosResponse } from "axios";
 import styled from "styled-components";
 import MatchStatWrapper from "./MatchStatWrapper";
 import ChampionImage from "./ChampionImage";
 import Result from "./Result";
+import { trackPromise } from "react-promise-tracker";
 
 interface StyleProps {
     win?: boolean;
@@ -20,6 +21,7 @@ const StyledMatchDiv = styled.div`
     border-radius: 4px;
     background-color: #182031;
     margin: 0.1rem;
+    justify-content: space-between;
 
     &::before {
         content: "";
@@ -37,35 +39,23 @@ interface RawMatchData {
 }
 
 export const Match: React.FC<RawMatchData> = ({ match, champions, matchTypes }) => {
-    const [matchSpecific, setMatchSpecific] = useState<MatchSpecific | null>();
-    const [playerStats, setPlayerStats] = useState<Participant | null>();
+    const [playerStats, setPlayerStats] = useState<SortedMatchData | null>();
     const [championName, setChampionName] = useState<string | null>();
     const [matchType, setMatchType] = useState<QueueTypes | null>();
     const [region, setRegion] = useContext(RegionContext);
 
     useEffect(() => {
         const fetch = (): void => {
-            axios
-                .get(matchSpecificEndpoint + region + "/" + match.gameId)
-                .then((response: AxiosResponse<MatchSpecific>) => {
-                    setMatchSpecific(response.data);
-                    getPlayerStats(response.data);
-                    getChampionName();
-                    getMatchType(response.data);
-                });
+            trackPromise(
+                axios.get(`${matchSpecificEndpoint + region}/${match.gameId}?championId=${match.champion}`)
+            ).then((response: AxiosResponse<SortedMatchData>) => {
+                setPlayerStats(response.data);
+                getMatchType(response.data);
+                getChampionName();
+            });
         };
         fetch();
     }, []);
-
-    const getPlayerStats = (response: MatchSpecific): void => {
-        const playerChampId: number = match.champion;
-
-        for (let participant of response.participants) {
-            if (participant.championId === playerChampId) {
-                setPlayerStats(participant);
-            }
-        }
-    };
 
     const getChampionName = (): void => {
         const championId: number = match.champion;
@@ -77,7 +67,7 @@ export const Match: React.FC<RawMatchData> = ({ match, champions, matchTypes }) 
         }
     };
 
-    const getMatchType = (response: MatchSpecific): void => {
+    const getMatchType = (response: SortedMatchData): void => {
         let queueId = response.queueId;
 
         for (let queueType of Object.values(matchTypes)) {
@@ -87,21 +77,45 @@ export const Match: React.FC<RawMatchData> = ({ match, champions, matchTypes }) 
         }
     };
 
+    const fixedGameLengthSeconds = (seconds: number) => {
+        if (seconds < 10) {
+            return `0${seconds}`;
+        }
+
+        return seconds;
+    };
+
     return (
-        <StyledMatchDiv win={playerStats?.stats.win}>
-            {matchSpecific && playerStats && championName && matchType && (
+        <StyledMatchDiv win={playerStats?.win}>
+            {playerStats && championName && matchType && (
                 <Fragment>
                     <ChampionImage champion={championName} />
                     <MatchStatWrapper>
-                        <Result win={playerStats.stats.win} />
+                        <Result win={playerStats.win} />
                     </MatchStatWrapper>
                     <MatchStatWrapper>
-                        <p>{playerStats.stats.kills}</p>
+                        <p>{playerStats.kda} KDA</p>
+                        <p>
+                            {playerStats.kills} / {playerStats.deaths} / {playerStats.assists}{" "}
+                        </p>
+                    </MatchStatWrapper>
+                    <MatchStatWrapper>
+                        <p>{playerStats.visionScorePerMin} Vis/min</p>
+                        <p>{playerStats.killParticipation}% KP</p>
+                    </MatchStatWrapper>
+                    <MatchStatWrapper>
+                        <p>{playerStats.csPerMin} CS/min</p>
+                        <p>{playerStats.minionsKilled} CS</p>
+                    </MatchStatWrapper>
+                    <MatchStatWrapper>
+                        <p>{playerStats.damagePerMin} DMG/min</p>
+                        <p>{playerStats.damagePercentOfTotal}% of team</p>
                     </MatchStatWrapper>
                     <MatchStatWrapper>
                         <p>
-                            {calculateKDA(playerStats.stats.kills, playerStats.stats.assists, playerStats.stats.deaths)}
+                            {playerStats.gameLength.minutes}:{fixedGameLengthSeconds(playerStats.gameLength.seconds)}
                         </p>
+                        <p>{matchType.description}</p>
                     </MatchStatWrapper>
                 </Fragment>
             )}
