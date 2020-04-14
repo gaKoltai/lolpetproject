71489341e732065fd0a337a32312184322b6584a
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-14 18:49:45 +0200

    Changed import to the correct location

--- src/components/index/LandingPage.tsx
+++ src/components/index/LandingPage.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import styled from "styled-components";
 import SearchWrapper from "../search/SearchWrapper";
-import { Title } from "../Title";
+import { Title } from "./Title";
 import { SummonerDataContext } from "../context/SummonerDataProvider";
 import Hero from "./Hero";
 
